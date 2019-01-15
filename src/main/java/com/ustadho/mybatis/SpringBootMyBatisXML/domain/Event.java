bb08cd1c4ed3da76b194712fb355c48547736c39
ustadho <cak.ust@gmail.com>
2019-01-15 14:09:16 +0700

    Implement Lombok

--- src/main/java/com/ustadho/mybatis/SpringBootMyBatisXML/domain/Event.java
+++ src/main/java/com/ustadho/mybatis/SpringBootMyBatisXML/domain/Event.java
@@ -1,7 +1,10 @@
 package com.ustadho.mybatis.SpringBootMyBatisXML.domain;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class Event {
     private long id;
     private String title;
@@ -10,51 +13,4 @@ public class Event {
     private Date endDate;
     private boolean isActive;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDetail() {
-        return detail;
-    }
-
-    public void setDetail(String detail) {
-        this.detail = detail;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startData) {
-        this.startDate = startData;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
 }
--- src/main/java/com/ustadho/mybatis/SpringBootMyBatisXML/domain/Program.java
+++ src/main/java/com/ustadho/mybatis/SpringBootMyBatisXML/domain/Program.java
@@ -1,22 +1,9 @@
 package com.ustadho.mybatis.SpringBootMyBatisXML.domain;
 
+import lombok.Data;
+
+@Data
 public class Program {
     private int id;
     private String name;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
